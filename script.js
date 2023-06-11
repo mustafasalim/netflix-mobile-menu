d6f4e0710a3b80062d126ef0b83007185d690f78
Mustafa Salim <salimmustafa763@gmail.com>
2023-06-11 15:22:43 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const headerIcon = document.querySelector(".header-icon")
+const headerMenu = document.querySelector(".header-menu")
+
+
+
+
+
+
+
+
+headerIcon.addEventListener("click",() => {
+    headerMenu.classList.toggle("action")
+})
